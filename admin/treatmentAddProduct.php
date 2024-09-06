f36d2c56994b99a2e0ba69a47724cf3d24b47b06
berti <berti@CLASSES.LOCAL>
2024-09-06 10:41:46 +0200

    treatmentAddProduct v2

--- admin/treatmentAddProduct.php
+++ admin/treatmentAddProduct.php
@@ -22,7 +22,7 @@
             $categories = ["Cat1","Cat2","Cat3"];
             if(in_array($_POST['categorie'],$categories))
             {
-                $categories = htmlspecialchars($_POST['categorie']);
+                $categorie = htmlspecialchars($_POST['categorie']);
             }else{
                 $err = 3;
             }
@@ -51,6 +51,56 @@
 
         if($err == 0)
         {
+              // ok - traitement du ou des fichier(s)
+              $dossier = "../images/"; // ../images/monfichier.jpg
+              $fichier = basename($_FILES['fichier']['name']);
+              $taille_maxi = 2000000;
+              $taille = filesize($_FILES['fichier']['tmp_name']);
+              $extensions = ['.png','.jpg','.jpeg'];
+              $extension = strrchr($_FILES['fichier']['name'],'.');
+            
+              if(!in_array($extension, $extensions))
+                {
+                    $error = 1;
+                }
+                
+                if($taille>$taille_maxi){
+                    $error = 2;
+                }
+            if(!isset($error))
+            {
+                $fichier = strtr($fichier, 
+                'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+                'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+                $fichier = preg_replace('/([^.a-z0-9]+)/i','-',$fichier); 
+                $fichiercptl = rand().$fichier; 
+
+
+                // c:/wamp64/tmp/FILE1236.tmp , "../images/1565156carole.jpg"
+                if(move_uploaded_file($_FILES['fichier']['tmp_name'],$dossier.$fichiercptl))
+                {
+                    //insertion dans la bdd
+                    require "../connexion.php";
+                    $insert = $bdd->prepare("INSERT INTO products(nom,categorie,fichier,description,date,prix) VALUES(:nom,:categorie,:fichier,:description,:date,:prix)");
+                    $insert->execute([
+                        ":nom" => $nom,
+                        ":categorie" => $categorie,
+                        ":fichier" => $fichiercptl,
+                        ":description"=> $description,
+                        ":date" => $date,
+                        ":prix" => $prix
+                    ]);
+                    $insert->closeCursor();
+                    header("LOCATION:products.php?add=success");
+                }else{
+                    header("LOCATION:addProduct.php?errorimg=3");
+                }
+
+
+            }
+            else{
+                header("LOCATION:addProduct.php?errorimg=".$error);
+            }
 
         }else{
             header("LOCATION:addProduct.php?error=".$err);
